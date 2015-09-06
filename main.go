494b2c166c0f2ddda3a741d10efd10f21a8b2004
Keiji, Yoshimi <walf443@gmail.com>
2015-09-06 23:10:02 +0900

    wrote buffering.

--- main.go
+++ main.go
@@ -4,18 +4,37 @@ import (
 	"flag"
 	"bufio"
 	"os"
-	"fmt"
 	"log"
+	"time"
+	"fmt"
 )
 
 
 func main() {
 	flag.Parse()
 	scanner := bufio.NewScanner(os.Stdin)
+	ch := make(chan string)
+	go background(ch)
 	for scanner.Scan() {
-		fmt.Println(scanner.Text())
+		ch <- scanner.Text()
 	}
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
+	}
+}
+
+func background(input chan string) {
+	buffer := make([]string, 0)
+	timer := time.Tick(200 * time.Millisecond)
+	for {
+		select {
+		case line := <-input:
+			buffer = append(buffer, line)
+		case <-timer:
+			for _, buf := range(buffer) {
+				fmt.Println(buf)
+			}
+			buffer = buffer[:0]
+		}
 	}
 }
